d92b7ea514b0fcc072d26ed824a77a7951e0e5fc
devaansh100 <devaansh.gupta@ntu.edu.sg>
2024-07-19 15:44:25 +0800

    bug fix in h()

--- src/algorithm/stp.py
+++ src/algorithm/stp.py
@@ -36,20 +36,11 @@ class AStar_stp(AStar):
 		self.goal = convert_array_to_stp(self.goal)
 
 	def h(self, node):
-		# if node.parent is None:
 		h = 0
 		for i in range(self.size[0]):
 			for j in range(self.size[1]):
-				h += manhattan_distance((i, j), self.goal_dict[node.pos[i, j]])
-		# else:
-		# 	x1, y1 = node.empty_pos
-		# 	x2, y2 = node.parent.empty_pos
-		# 	# For parent, (x2, y2) is blank and (x1, y1) is num
-		# 	# For child, (x1, y1) is blank and (x2, y2) is num
-		# 	assert (node.parent.pos[x1, y1] == node.pos[x2, y2]) and (node.parent.pos[x2, y2] == node.pos[x1, y1] == 0)
-		# 	num = node.pos[x2, y2].item()
-		# 	h = node.parent.h - manhattan_distance((x1, y1), self.goal_dict[num]) - manhattan_distance((x2, y2), self.goal_dict[0]) \
-		# 					  + manhattan_distance((x1, y1), self.goal_dict[0]) + manhattan_distance((x2, y2), self.goal_dict[num])
+				if node.pos[i, j] != 0:
+					h += manhattan_distance((i, j), self.goal_dict[node.pos[i, j]])
 		return h
 
 	def display(self):
@@ -76,7 +67,6 @@ class AStar_stp(AStar):
 				child_puzzle = node.pos.copy()
 				child_puzzle[child_pos], child_puzzle[node.empty_pos] = child_puzzle[node.empty_pos], child_puzzle[child_pos]
 				child = Node_stp(child_puzzle, g = node.g + 1)
-				child.parent = node # NOTE: This is being overwritten by the same value later. It is set here once to perform incremental calculations forh
 				children.append(child)
 		children = self.populate_h(children)
 		return children
