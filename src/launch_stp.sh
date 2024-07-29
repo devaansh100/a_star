9aaa696f711ccc6656d999af96cae8bde65a8ce4
devaansh100 <devaansh.gupta@ntu.edu.sg>
2024-07-29 17:19:02 +0800

    final

--- src/launch_stp.sh
+++ src/launch_stp.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 if [ $1 = "lm" ]; then
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --train-files alg_stp_3_$2_8 --val-files alg_stp_3_$2_5 --base-model $3
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --lm model_best_test.pth --test-ilr test stp_3 --base-model $3 --train-files
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --lm model_best_test.pth --test-ilr test stp_4 stp_5 --base-model $3 --train-files
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --train-files alg_stp_3_$2_8 --val-files alg_stp_3_$2_5 --base-model $3
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --lm model_best_test.pth --test-ilr test stp_3 --base-model $3 --train-files
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --lm model_best_test.pth --test-ilr test stp_4 stp_5 --base-model $3 --train-files
 fi
 
 if [ $1 = "l2" ]; then
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --train-files alg_stp_3_$2_8 --val-files alg_stp_3_$2_5 --loss l2 --base-model $3
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --lm model_best_test_l2_1.pth --test-ilr test stp_3 --loss l2 --base-model $3 --train-files
-    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2-comb --lm model_best_test_l2_1.pth --test-ilr test stp_4 stp_5 --loss l2 --base-model $3 --train-files
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --train-files alg_stp_3_$2_8 --val-files alg_stp_3_$2_5 --loss l2 --base-model $3
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --lm model_best_test_l2_1.pth --test-ilr test stp_3 --loss l2 --base-model $3 --train-files
+    python main.py --domain stp --dataset stp-alph --job fin-stp-alph-$2 --lm model_best_test_l2_1.pth --test-ilr test stp_4 stp_5 --loss l2 --base-model $3 --train-files
 fi
--- src/run.sh
+++ src/run.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 base_model="code-t5"
-sample="semdedup-f"
-sample_name="semdedup-f"
-tmux new-session -t "comb-stp_l2" -d -s
-tmux send-keys -t "comb-stp_l2" "conda activate reasoners" C-m
-tmux send-keys -t "comb-stp_l2" "CUDA_VISIBLE_DEVICES=0 ./launch_stp.sh l2 $sample $base_model" C-m
+sample="semdedup"
+sample_name="semdedup"
+tmux new-session -t "stp_lm_${sample_name}" -d -s
+tmux send-keys -t "stp_lm_${sample_name}" "conda activate reasoners" C-m
+tmux send-keys -t "stp_lm_${sample_name}" "CUDA_VISIBLE_DEVICES=1 ./launch_stp.sh lm $sample $base_model" C-m
 
-tmux new-session -t "comb-maze_l2" -d -s
-tmux send-keys -t "comb-maze_l2" "conda activate reasoners" C-m
-tmux send-keys -t "comb-maze_l2" "CUDA_VISIBLE_DEVICES=1 ./launch_maze.sh l2 $sample $base_model" C-m
+# sample="optimal_dist_lin_2.0"
+# sample_name="optimal_dist_lin_2"
+# tmux new-session -t "maze_l2_${sample_name}" -d -s
+# tmux send-keys -t "maze_l2_${sample_name}" "conda activate reasoners" C-m
+# tmux send-keys -t "maze_l2_${sample_name}" "CUDA_VISIBLE_DEVICES=1 ./launch_maze.sh l2 $sample $base_model" C-m
 
-tmux new-session -t "comb-sokoban_l2" -d -s
-tmux send-keys -t "comb-sokoban_l2" "conda activate reasoners" C-m
-tmux send-keys -t "comb-sokoban_l2" "CUDA_VISIBLE_DEVICES=2 ./launch_sokoban.sh l2 $sample $base_model" C-m
+# sample="optimal_dist_lin_0.8"
+# sample_name="optimal_dist_lin_0_8"
+# tmux new-session -t "sokoban_l2_${sample_name}" -d -s
+# tmux send-keys -t "sokoban_l2_${sample_name}" "conda activate reasoners" C-m
+# tmux send-keys -t "sokoban_l2_${sample_name}" "CUDA_VISIBLE_DEVICES=2 ./launch_sokoban.sh l2 $sample $base_model" C-m
 
 
 # sample="optimal_dist_12.0"
